3386e8e9b5be388d9070285111f2d73d203461be
cheng <807992213@qq.com>
2019-10-09 19:04:31 +0800

    oauth2

---
+++ oauth2/oauth2server/src/main/java/com/sdstc/controller/PubController.java
@@ -0,0 +1,27 @@
+package com.sdstc.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PubController {
+
+	@GetMapping("/admin")
+	public Map<String, String> admin() {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("x", "y");
+		return result;
+	}
+	
+	@GetMapping("/user")
+	public Map<String, String> user() {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("x", "y");
+		return result;
+	}
+}
