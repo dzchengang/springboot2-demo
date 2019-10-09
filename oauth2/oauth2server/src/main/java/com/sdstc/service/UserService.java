0f90b65f355641759e3687d3a69d875355bf1da8
cheng <cheng@cheng-PC>
2019-10-09 23:45:17 +0800

    oauth2

--- oauth2/oauth2server/src/main/java/com/sdstc/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.sdstc.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserService {
-	UserDetails getUserDetailByPhone(String phone);
-}
--- oauth2/oauth2server/src/main/java/com/sdstc/service/impl/UserDetailsServiceImpl.java
+++ oauth2/oauth2server/src/main/java/com/sdstc/service/impl/UserDetailsServiceImpl.java
@@ -8,24 +8,24 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.sdstc.service.UserService;
-
-@Service("userService")
-public class UserServiceImpl implements UserService {
+@Service("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
 	@Override
-	public UserDetails getUserDetailByPhone(String phone){
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = null;
-		if (phone.equals("admin")) {
+		if (username.equals("admin")) {
 			List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 			user = new User("admin", passwordEncoder.encode("111111"), authorities);
-		} else if (phone.equals("user")) {
+		} else if (username.equals("user")) {
 			List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 			authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 			user = new User("user", passwordEncoder.encode("111111"), authorities);
