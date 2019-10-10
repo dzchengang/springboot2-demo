0b5d09172c9dcf81e6c1d301172e69f5598ac004
cheng <807992213@qq.com>
2019-10-10 08:52:21 +0800

    1

--- oauth2/oauth2server/src/main/java/com/sdstc/config/AuthorizationServerConfiguration.java
+++ oauth2/oauth2server/src/main/java/com/sdstc/config/AuthorizationServerConfiguration.java
@@ -1,16 +1,22 @@
 package com.sdstc.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
-
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		super.configure(security);
@@ -21,9 +27,21 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 		super.configure(clients);
 	}
 
+	
+	
+	
+
+	 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		super.configure(endpoints);
+		TokenStore tokenStore=null;
+		endpoints
+        .tokenStore(tokenStore)
+        .authenticationManager(authenticationManager)
+        .userDetailsService(userDetailsService)
+        // 2018-4-3 增加配置，允许 GET、POST 请求获取 token，即访问端点：oauth/token
+        .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+
 	}
 
 }
