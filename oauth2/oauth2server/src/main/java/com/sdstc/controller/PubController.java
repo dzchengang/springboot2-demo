76a9486ef32aabfd8a1fc8ef305185b6d76c21f4
cheng <807992213@qq.com>
2019-11-14 15:46:11 +0800

    1

--- oauth2/oauth2server/src/main/java/com/sdstc/controller/PubController.java
+++ oauth2/oauth2server/src/main/java/com/sdstc/controller/PubController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
