3a90b67b414352dec52156b085d2d6f7f798dc07
cheng <807992213@qq.com>
2019-11-17 12:52:55 +0800

    1

--- oauth2/oauth2client/src/main/java/com/sdstc/controller/PubController.java
+++ oauth2/oauth2client/src/main/java/com/sdstc/controller/PubController.java
@@ -3,6 +3,7 @@ package com.sdstc.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PubController {
 
 	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public Map<String, String> admin() {
 		Map<String, String> result = new HashMap<String, String>();
 		result.put("x", "y");
@@ -19,6 +21,7 @@ public class PubController {
 	}
 	
 	@GetMapping("/user")
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public Map<String, String> user() {
 		Map<String, String> result = new HashMap<String, String>();
 		result.put("x", "y");
